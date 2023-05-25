c7221e42128de2344fdb3cc8fd19c831b41869e5
iamNandan <nandanhegde.is20@bmsce.ac.in>
2023-05-25 17:17:41 +0530

    Add gif

--- main.py
+++ main.py
@@ -93,11 +93,11 @@ def render_about_page():
     st.write(
         '<style>div.block-container{padding-top:0.8rem;}</style>', unsafe_allow_html=True)
     st.title("About Us")
-    st.write("At SentimentX, we are passionate about sentiment analysis and its potential to unlock valuable insights from text data. Sentiment analysis, also known as opinion mining, is the process of determining the emotional tone or sentiment expressed in a piece of text.")
+    st.write("At EmoTracker, we are passionate about sentiment analysis and its potential to unlock valuable insights from text data. Sentiment analysis, also known as opinion mining, is the process of determining the emotional tone or sentiment expressed in a piece of text.")
     st.write("Our website aims to simplify sentiment analysis and make it accessible to everyone. With our advanced natural language processing algorithms, we analyze text data to identify and quantify sentiments such as positive, negative, or neutral. Whether it's customer reviews, social media posts, or any other text-based data, our platform provides valuable insights that can be used to make data-driven decisions and gain a deeper understanding of public opinion.")
-    st.write("With SentimentX, you can easily analyze the sentiment of text data, visualize sentiment trends, and extract key insights. Our user-friendly interface and powerful analysis tools empower individuals and businesses to harness the power of sentiment analysis and leverage it for various applications, such as brand monitoring, customer feedback analysis, market research, and more.")
+    st.write("With EmoTracker, you can easily analyze the sentiment of text data, visualize sentiment trends, and extract key insights. Our user-friendly interface and powerful analysis tools empower individuals and businesses to harness the power of sentiment analysis and leverage it for various applications, such as brand monitoring, customer feedback analysis, market research, and more.")
     st.write("Join us on this exciting journey as we uncover the sentiments hidden within the vast ocean of text data and help you derive meaningful value from it.")
-
+    #Sentiment x
     st.title("Meet the Team")
 
     from PIL import Image, ImageDraw, ImageOps
